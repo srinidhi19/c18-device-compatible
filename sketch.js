09f29819936bd232dd4edd686c181f903c0edf17
srinidhi19 <89329602+srinidhi19@users.noreply.github.com>
2021-09-12 10:05:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var s1
+
+function setup()
+ {
+  createCanvas(400,400);
+  s1 = new Student("john,12,16");
+  s1.display()
+
+}
+function draw() 
+{
+  background(51);
+  
+}
---
+++ student.js
@@ -0,0 +1,22 @@
+class Student
+
+{
+   constructor(name,age,grade)
+   {
+       this.name = name;
+       this.grade = grade;
+       this.age = age;
+   }
+
+   display()
+   {
+       console.log(this.name);
+       console.log(this.age);
+       console.log(this.grade);
+   }
+
+   
+}
+
+
+
